1d5892fb0950f642204084c9bb3a70f9f7723436
venkatthirupathi <thirupathivenkat2005@gmail.com>
2025-04-19 19:18:31 +0530

    after downloadbutton sessionsharing

--- server.js
+++ server.js
@@ -1,3 +1,84 @@
+// // this is server side
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const path = require('path');
+// const { Server } = require('socket.io');
+// const ACTIONS = require('./src/Actions');
+
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// app.use(express.static('build'));
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// const userSocketMap = {};
+// function getAllConnectedClients(roomId) {
+//     // Map
+//     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+//         (socketId) => {
+//             return {
+//                 socketId,
+//                 username: userSocketMap[socketId],
+//             };
+//         }
+//     );
+// }
+
+// io.on('connection', (socket) => {
+//     console.log('socket connected', socket.id);
+
+//     socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+//         userSocketMap[socket.id] = username;
+//         socket.join(roomId);
+//         const clients = getAllConnectedClients(roomId);
+//         clients.forEach(({ socketId }) => {
+//             io.to(socketId).emit(ACTIONS.JOINED, {
+//                 clients,
+//                 username,
+//                 socketId: socket.id,
+//             });
+//         });
+//     });
+
+//     socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+//         socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+//     });
+
+//     socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+//         io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+//     });
+
+//     socket.on('disconnecting', () => {
+//         const rooms = [...socket.rooms];
+//         rooms.forEach((roomId) => {
+//             socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+//                 socketId: socket.id,
+//                 username: userSocketMap[socket.id],
+//             });
+//         });
+//         delete userSocketMap[socket.id];
+//         socket.leave();
+//     });
+// });
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
 // this is server side
 const express = require('express');
 const app = express();
@@ -7,7 +88,12 @@ const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*", // Adjust this in production for security
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
@@ -16,20 +102,18 @@ app.use((req, res, next) => {
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
-    // Map
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
+        (socketId) => ({
+            socketId,
+            username: userSocketMap[socketId],
+        })
     );
 }
 
 io.on('connection', (socket) => {
     console.log('socket connected', socket.id);
 
+    // Join code collaboration room
     socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
         userSocketMap[socket.id] = username;
         socket.join(roomId);
@@ -51,6 +135,18 @@ io.on('connection', (socket) => {
         io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
     });
 
+    // 📹 Video call signaling
+    socket.on('call-user', ({ userToCall, signalData, from, name }) => {
+        console.log(`Calling user ${userToCall} from ${from}`);
+        io.to(userToCall).emit('call-user', { signal: signalData, from, name });
+    });
+
+    socket.on('answer-call', ({ to, signal }) => {
+        console.log(`Answering call from ${to}`);
+        io.to(to).emit('call-accepted', signal);
+    });
+
+    // Disconnecting
     socket.on('disconnecting', () => {
         const rooms = [...socket.rooms];
         rooms.forEach((roomId) => {
@@ -60,9 +156,8 @@ io.on('connection', (socket) => {
             });
         });
         delete userSocketMap[socket.id];
-        socket.leave();
     });
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}`));
